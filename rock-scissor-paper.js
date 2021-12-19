49a67a651b8d314ed9e5fcd7011099d5eb0b727f
lay <laylafkamilos@gmail.com>
2021-12-19 20:57:11 -0300

    Edit rock-scissor-paper.js user input

--- rock-scissor-paper.js
+++ rock-scissor-paper.js
@@ -1,18 +1,21 @@
-//get random choice by the computer:
-/* get the "computerPlay" select randomsly between rock, scissor and paper;
-    set a list array [rock, scissor, paper, rock, scissor, paper, rock, scissor, paper].
-    Chose random between 0 and 9.
-    return rock scissor, paper.
+//get player one choice and compare.
+function getPlayerOneChoice () { 
+    let playChoice = prompt("Choose between rock, scissor or paper:","");
+    playChoice = playChoice.toLowerCase().trim();
+    return playChoice;
+}
 
-    //console.log(computerPlay); */
-
-
-
+const playerOne = getPlayerOneChoice();
 
 //functions for computerPlay to work
+function getRandomInt (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
 function getRandomPlay () {
-    let playChoice = ['rock', 'scissor', 'paper', 'rock', 'scissor', 'paper','rock', 'scissor', 'paper'];
+    let playChoice = ['rock', 'scissor', 'paper'];
     //get random number between array lenght.
     const maxNumber = playChoice.length;
     //get random index array and return it
@@ -20,12 +23,16 @@ function getRandomPlay () {
     return playChoice;
 }
 
-
 const computerPlay = getRandomPlay();
-console.log(computerPlay);
 
-function getRandomInt (min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
+//compare the choices:
+
+
+
+
+console.log(`This is the computer choice: ${computerPlay}`); // just for checking
+console.log(`This is the playerOne choice: ${playerOne}`); // just for checking
+
+//Try the loop later;
+// (playChoice !== 'rock' || playChoice !== 'scissor' || playChoice !== 'paper')
+// (playerOne !== 'rock' || playerOne !== 'scissor' || playerOne !== 'paper')
