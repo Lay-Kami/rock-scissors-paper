8aeadba4af83d6af4ba42847d25626dd13ef7b5b
lay <laylafkamilos@gmail.com>
2021-12-20 17:56:09 -0300

    Add loop match to rock-scissor-paper.js

--- rock-scissor-paper.js
+++ rock-scissor-paper.js
@@ -5,8 +5,8 @@ function getPlayerOneChoice () {
     do {
         playChoice = prompt("Choose between rock, scissor or paper:","");
         playChoice = playChoice.toLowerCase().trim();
-    } while ((playChoice !== 'rock') && (playChoice !== 'scissor') 
-    && (playChoice !== 'paper'));
+    } while ((playChoice !== 'rock') && (playChoice !== 'scissor') && 
+    (playChoice !== 'paper'));
     return playChoice;
 }
 //functions for computerPlay to work
@@ -17,41 +17,52 @@ function getRandomInt (min, max) {
 }
 function getRandomPlay () {
     let playChoice = ['rock', 'scissor', 'paper'];
-    //get random number between array lenght.
     const maxNumber = playChoice.length;
     //get random index array and return it
     playChoice = playChoice.at(getRandomInt(0, maxNumber));
     return playChoice;
 }
-//create a game() with arguments of the computer and player:
-function getGameMatch(playerSelection, computerSelection) {
-    let playerScore = 0;
-    let computerScore = 0;
+//create a game() with arguments of the computerPlay and playerPlay:
+function getGameMatch(playerSelection = getPlayerOneChoice(), 
+computerSelection = getRandomPlay(), score_01 = 0, score_02 = 0) {
+    //set initial score
+    let playerScore = score_01;
+    let computerScore = score_02;
+    //test game matchs
     if (playerSelection == computerSelection) {
         console.log("Draw");
         playerScore++;
         computerScore++;
         return [playerScore, computerScore];
-    } else if (
-      (playerSelection == 'rock' && computerSelection == 'scissors') || 
-      (playerSelection == 'scissor' && computerSelection == 'paper') || 
-      (playerSelection == 'paper' && computerSelection == 'rock')) {
+    } else if ((playerSelection == 'rock' && computerSelection == 'scissor') 
+    || (playerSelection == 'scissor' && computerSelection == 'paper') 
+    || (playerSelection == 'paper' && computerSelection == 'rock')) {
         console.log(`Win, ${playerSelection} beats ${computerSelection}`);
         playerScore++;
         return [playerScore, computerScore];
     } else {
-        console.log(`Lose, ${playerOne} loses against ${computerPlay}`);
+        console.log(`Lose, ${playerSelection} loses against ${computerSelection}`);
         computerScore++;
         return [playerScore, computerScore];
     }
+} //return output expected: [1,0]; [1,1]; [0,0]
+//hold player score
+const playGameMatch = (rounds = 5) => {
+    let playerOneScore = 0;
+    let computerPlayScore = 0;
+    let matchResult;
+    //get match result and score
+    for (let i = 0; i < rounds; i++) {
+        matchResult = getGameMatch();
+        playerOneScore = playerOneScore + matchResult[0];
+        computerPlayScore = computerPlayScore + matchResult[1];
+        console.log(`match score is: ${playerOneScore} vs ${computerPlayScore}`);
+    }
+    console.log(`final match score is: ${playerOneScore} vs ${computerPlayScore}`);
+    return;
 }
-const playerOne = getPlayerOneChoice();
-const computerPlay = getRandomPlay();
 
-let matchResult = getGameMatch(playerOne, computerPlay);
-let playerOneScore = matchResult[0];
-let computerPlayScore = matchResult[1];
-
-console.log(`match score is: ${playerOneScore} vs ${computerPlayScore}`);
-console.log(`This is the playerOne choice: ${playerOne}`); // just for checking
-console.log(`This is the computer choice: ${computerPlay}`); // just for checking
+playGameMatch();
+//2 vs 2
+// console.log(`This is the playerOne choice: ${playerOne}`); // just for checking
+// console.log(`This is the computer choice: ${computerPlay}`); // just for checking
