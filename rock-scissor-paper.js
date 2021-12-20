23c5861c4b9bb1a7ad016f580b19c9a84e0c8d06
lay <laylafkamilos@gmail.com>
2021-12-20 12:16:58 -0300

    Edit gameMatch complete

--- rock-scissor-paper.js
+++ rock-scissor-paper.js
@@ -1,19 +1,20 @@
 //get player one choice and compare.
 function getPlayerOneChoice () { 
-    let playChoice = prompt("Choose between rock, scissor or paper:","");
-    playChoice = playChoice.toLowerCase().trim();
+    let playChoice;
+    do {
+        playChoice = prompt("Choose between rock, scissor or paper:","");
+        playChoice = playChoice.toLowerCase().trim();
+    } while ((playChoice !== 'rock') && (playChoice !== 'scissor') 
+    && (playChoice !== 'paper'));
     return playChoice;
 }
-
 const playerOne = getPlayerOneChoice();
-
 //functions for computerPlay to work
 function getRandomInt (min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
-
 function getRandomPlay () {
     let playChoice = ['rock', 'scissor', 'paper'];
     //get random number between array lenght.
@@ -22,27 +23,24 @@ function getRandomPlay () {
     playChoice = playChoice.at(getRandomInt(0, maxNumber));
     return playChoice;
 }
-
 const computerPlay = getRandomPlay();
-
 //create a game() with arguments of the computer and player:
-
-function gameMatch(playerOneSelection, computerSelection) {
-    //some code..
-
-    return //if win match
-    return //if draw
-    return //if lose;
+function getGameMatch(playerSelection, computerSelection) {
+    //ternary operator trainnig:
+    const isMatchResult = (playerSelection == computerSelection) ? `draw` 
+    : (playerSelection == 'rock' && computerSelection == 'scissors') || 
+      (playerSelection == 'scissor' && computerSelection == 'paper') || 
+      (playerSelection == 'paper' && computerSelection == 'rock') ? `Win, ${playerSelection} beats ${computerSelection}`
+    : `Lose, ${playerOne} loses against ${computerPlay}`;
+    return isMatchResult;
 }
 
 
-
-
-
-
-console.log(`This is the computer choice: ${computerPlay}`); // just for checking
+console.log(getGameMatch(playerOne, computerPlay));
 console.log(`This is the playerOne choice: ${playerOne}`); // just for checking
+console.log(`This is the computer choice: ${computerPlay}`); // just for checking
+
 
 //Try the loop later;
-// (playChoice !== 'rock' || playChoice !== 'scissor' || playChoice !== 'paper')
+// (playChoice !== 'rock') || (playChoice !== 'scissor') || (playChoice !== 'paper')
 // (playerOne !== 'rock' || playerOne !== 'scissor' || playerOne !== 'paper')
